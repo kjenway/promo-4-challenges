51b187bb39a4b20927d8dc827423e2d10995ef06
Annabel <annabel.menard@gmail.com>
2014-10-07 19:13:36 +0200

    Exo 5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,21 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+  if your_message.include?("?")
+    return "Silly question, get dressed and go to work!"
+  elsif your_message == "I am going to work right now!"
+    return ''
+  else
+    return "I don't care, get dressed and go to work!"
+  end
 end
+
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
-end
+  prefix = ''
+  if your_message == "I am going to work right now!".upcase
+    return ''
+  elsif your_message == your_message.upcase
+    prefix = "I can feel your motivation! "
+  end
 
+  return prefix + coach_answer(your_message)
+end
