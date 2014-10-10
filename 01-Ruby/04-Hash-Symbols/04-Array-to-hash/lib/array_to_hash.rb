68c935f8c6be41eafa6834377e092c943996292a
Annabel <annabel.menard@gmail.com>
2014-10-10 17:55:19 +0200

    Exo 4

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -5,10 +5,12 @@ def array_to_hash(array)
   #TODO: implement the method :)
   indexes = (0...array.count)
   if block_given?
-    indexes2 = 0
-    indexes.map {|index| yield(index)}
-  end
+    indexes2 = []
+    indexes.each {|index| indexes2 << yield(index)}
+    return indexes2.zip(array).to_h
+  else
     hash = indexes.zip(array).to_h
+  end
 end
 
 p array_to_hash(exemple) {|index| "key#{index + 1}"}
