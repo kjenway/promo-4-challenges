070bb7f9a69769ddf0e890ff3e57ea5ec04837f5
Annabel <annabel.menard@gmail.com>
2014-10-13 17:41:20 +0200

    Exo3

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -1,23 +1,17 @@
-# Encoding: utf-8
-
 def mail_joke(email)
-  # TODO: Return (not print!) a joke suited to the email provided
-  if email.class != String
-    raise ArgumentError.new("This is not an accepted email format, it must be a string")
-  elsif email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
-    provider = email.match(/@(\w+)/)[1].downcase
-    provider_complete = email.match(/@(\w+.+)$/)[1].downcase
+  if email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i).nil?
+    fail ArgumentError, "This is not an accepted email format, it must be a string"
+  else
+    provider = email.match(/@(\w+.+)$/)[1].downcase
     name = email.match(/^(\w+.+)@/)[1].downcase.gsub(".", " ")
-    if provider == "lewagon"
+    if provider == "lewagon.org"
       return "Well done #{name}, you're skilled and capable"
-    elsif provider == "gmail"
+    elsif provider == "gmail.com"
       return "#{name}, you're an average but modern person"
-    elsif provider == "live"
+    elsif provider == "live.com"
       return "#{name}, aren't you born before 1973?"
     else
-      return "Sorry #{name}, we don't know how to judge '#{provider_complete}'"
+      return "Sorry #{name}, we don't know how to judge '#{provider}'"
     end
-  else
-    raise ArgumentError.new("This is not an accepted email format, please check your entry")
   end
 end
