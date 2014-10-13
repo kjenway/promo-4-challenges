77756fce8dcd44886354c2029125130631fa2584
Annabel <annabel.menard@gmail.com>
2014-10-13 15:32:16 +0200

    Exo2

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,33 +1,21 @@
 def anagrams?(a_string, another_string)
-
- a_string = a_string.gsub(/[\W]/, "").downcase
- another_string = another_string.gsub(/[\W]/, "").downcase
-
- if a_string.chars.sort.join == another_string.chars.sort.join
-   return true
- else
-   return false
- end
-
+  a_string = a_string.gsub(/[\W]/, "").downcase
+  another_string = another_string.gsub(/[\W]/, "").downcase
+  if a_string.chars.sort.join == another_string.chars.sort.join
+    return true
+  else
+    return false
+  end
 end
 
-
-
 def anagrams_on_steroids?(a_string, another_string)
-  #TODO: implement the improved method
- a_string = a_string.gsub(/[\W]/, "").downcase
- another_string = another_string.gsub(/[\W]/, "").downcase
-
- hash1 = {}
- a_string.chars.each {|i| hash1[i] = a_string.count(i)}
-
- hash2 = {}
- another_string.chars.each {|i| hash2[i] = a_string.count(i)}
-
- if hash1 == hash2
-  return true
- else
-  return false
- end
-
+  hash1 = {}
+  a_string.gsub(/[\W]/, "").downcase.chars.each { |i| hash1[i] = a_string.count(i) }
+  hash2 = {}
+  another_string.gsub(/[\W]/, "").downcase.chars.each { |i| hash2[i] = a_string.count(i) }
+  if hash1 == hash2
+    return true
+  else
+    return false
+  end
 end
