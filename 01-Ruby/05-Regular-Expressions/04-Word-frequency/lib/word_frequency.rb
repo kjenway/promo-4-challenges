e7c052909ea0d6159319ee747492df550a7f87d8
Annabel <annabel.menard@gmail.com>
2014-10-14 12:31:11 +0200

    Exo1

--- 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
@@ -1,3 +1,22 @@
 def most_common_words(file_name, stop_words_file_name, number_of_word)
   # TODO: return hash of occurences of number_of_word most frequent words
+  hash1 = Hash.new(0)
+  text = []
+  stopwords = []
+  File.open(file_name, "r").each_line do |line1|
+    text << line1.split(/\W+/)
+    text.flatten!
+  end
+  File.open(stop_words_file_name, "r").each_line do |line2|
+    stopwords << line2.split(/\W+/)
+    stopwords.flatten!
+  end
+  text_without_stopwords = text - stopwords
+  text_without_stopwords.each do |word|
+    hash1[word] += 1
+  end
+  result = hash1.sort_by { |word, num| num }.to_h
+  result.max_by{|k,v| v}
 end
+
+p most_common_words("source-text.txt","stop_words.txt",2)
--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,6 +1,20 @@
 # Encoding: utf-8
 require 'csv'
+require 'date'
 
 def most_successfull(number, max_year, file_name)
-  # TODO: return the number most successfull movies max_year
+  movies = []
+  movies_of_year = []
+  CSV.foreach(file_name) do |row|
+    movies << { name: row[0], year: row[1].to_i, earnings: row[2].to_i }
+  end
+  movies.each do|movie_hash|
+    if movie_hash[:year] < max_year
+      movies_of_year << movie_hash
+    end
+  end
+  movies_of_year.sort do |x, y|
+    x[:earnings] <=> y[:earnings]
+  end
+  movies_of_year.take(number)
 end
