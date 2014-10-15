04f3f53a4e3ff64f89a13fa4f226da909e1a25f1
Annabel <annabel.menard@gmail.com>
2014-10-15 11:19:05 +0200

    Exo1

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -1,12 +1,9 @@
 require 'open-uri'
 require 'json'
 
-
 def compute_score(attempt, time_taken)
-  (time_taken > 60) ? 0 : 3 / time_taken.to_i + attempt.size
+  (time_taken > 60) ? 0 : 8 / time_taken.to_i + attempt.size
 end
-
-# open(api_url) do
 
 def generate_grid(grid_size)
   origin = ('A'..'Z').to_a
@@ -18,13 +15,12 @@ def generate_grid(grid_size)
 end
 
 def get_translation(word)
-  api_url = open("http://api.wordreference.com/0.8/80143/json/enfr/#{word.upcase}")
+  api_url = open("http://api.wordreference.com/0.8/80143/json/enfr/#{word}")
   word = JSON.parse(api_url.read)
   word['term0']["PrincipalTranslations"]["0"]["FirstTranslation"]["term"] unless word['Error']
 end
 
 def run_game(attempt, grid, start_time, end_time)
-  # TODO: runs the game and return detailed hash of result
   time_taken = end_time - start_time
   result = { score: 0, time: time_taken.to_i }
   if attempt.upcase.chars.all? { |x| grid.include? x }
@@ -34,7 +30,6 @@ def run_game(attempt, grid, start_time, end_time)
       result[:score] = compute_score(attempt, time_taken)
     else
       result[:message] = "not an english word"
-      # result[:translation] = "No translation"
     end
   else
     result[:message] = "not in the grid"
@@ -42,22 +37,3 @@ def run_game(attempt, grid, start_time, end_time)
   end
   result
 end
-
-p run_game("wagon", %w(W G G Z O N A L), Time.now, Time.now + 1.0)
-
-# run_game("ye", ["y","e","s"], Time.now, Time.now )
-
-# puts "Your word: #{attempt}"
-# puts "Time Taken to answer: #{result[:time]}"
-# puts "Translation: #{result[:translation]}"
-# puts "Your score: #{result[:score]}"
-# puts "Message: #{result[:message]}"
-
-# def run_game(attempt, grid, start_time, end_time)
-#   api_url = 'http://api.wordreference.com/0.8/80143/json/enfr/apple'
-
-#   # TODO: runs the game and return detailed hash of result
-#   if attempt.all? {|x| grid.include? x}
-#     open(api_url) do |data|
-#     translation = JSON.parse(data.read)
-#     if translation["term0"]["PrincipalTranslations"]["0"]["OriginalTerm"]["term"] == attempt
