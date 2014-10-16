9927891a7901fd6da4199fe7107360bb25e9b0c4
Annabel <annabel.menard@gmail.com>
2014-10-16 17:11:09 +0200

    Optional 1

---
+++ 02-OOP/02-OO-Advanced/Optional-01-Web-Browser/lib/browser.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Browser
+
+  def fetch_content (url)
+    Nokogiri::HTML(open(url)) do |config|
+    config.strict.noent
+    end
+  end
+  # p Nokogiri::HTML(doc.to_s).text
+end
