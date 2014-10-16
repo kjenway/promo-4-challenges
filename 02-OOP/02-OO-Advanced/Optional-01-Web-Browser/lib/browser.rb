ff130ccf13dd82e0d2791d4a4f63e29c8a191afa
Annabel <annabel.menard@gmail.com>
2014-10-16 17:16:25 +0200

    Optional Exo1

--- 02-OOP/02-OO-Advanced/Optional-01-Web-Browser/lib/browser.rb
+++ 02-OOP/02-OO-Advanced/Optional-01-Web-Browser/lib/browser.rb
@@ -2,10 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 class Browser
-
-  def fetch_content (url)
+  def fetch_content(url)
     Nokogiri::HTML(open(url)) do |config|
-    config.strict.noent
+      config.strict.noent
     end
   end
   # p Nokogiri::HTML(doc.to_s).text
