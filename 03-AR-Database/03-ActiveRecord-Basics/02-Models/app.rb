94f792d7a93676e25771d02e673982a91a01bf52
Annabel <annabel.menard@gmail.com>
2014-10-27 17:40:08 +0100

    Exo4

--- 03-AR-Database/03-ActiveRecord-Basics/02-Models/app.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Models/app.rb
@@ -1,2 +1,4 @@
 require_relative "app/router"
+require_relative "config/application"
+
 Router.new.run
--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
@@ -1,4 +1,16 @@
+require_relative "../config/application"
+require_relative "../app/models/post"
 require "json"
 require "rest-client"
 
 # TODO: Write a seed to insert 100 posts in the database fetched from the Hacker News API.
+response = RestClient.get 'https://hacker-news.firebaseio.com/v0/topstories.json'
+ids = JSON.parse(response)
+
+ids[0..9].each do |id|
+  url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+  details = RestClient.get url
+  hash = JSON.parse(details)
+  post = Post.new(name: hash["title"], url: hash ["url"], votes: hash["votes"])
+  post.save
+end
