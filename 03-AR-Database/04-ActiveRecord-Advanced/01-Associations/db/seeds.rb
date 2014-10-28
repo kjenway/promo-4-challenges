d7dc1dda91f3dba85a67977ec5c9a48505558005
Annabel <annabel.menard@gmail.com>
2014-10-28 11:34:19 +0100

    Exo1

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,13 @@
 # TODO: Write a seed
+require 'faker'
+
+5.times do
+  user = User.new(
+      username: Faker::Internet.user_name,
+      email: Faker::Internet.email
+    )
+  user.save
+  (5..10).to_a.sample.times do
+    user.posts.create(name: Faker::Name.name, url: Faker::Internet.url, votes: (0..10).to_a.sample)
+  end
+end
