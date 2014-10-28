ad602f022b3befa00201e0217ce412206a0cd3a2
Annabel <annabel.menard@gmail.com>
2014-10-28 14:05:06 +0100

    Exo2

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
+  validates :name, length: { minimum: 5 }
+  validates :url, :user, :name, presence: true
+  validates :url, format: {
+    with: /\Ahttps?\:\/\/(?:www\.|)?\w+\.\w{2,3}(?:\/\w+)?/,
+    message: "invalid url"
+  }
+  validates :name, uniqueness: { case_sensitive: false }
   # TODO: Add some validation
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   has_many :posts
+  before_validation :stripspaces
+  validates :username, :email, presence: true
+  validates_uniqueness_of :username
+  validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/, message:
+  "invalid email" }
 
+  def stripspaces
+    self.email = email.lstrip.rstrip unless email.nil?
+  end
   # TODO: Add some validation
 end
