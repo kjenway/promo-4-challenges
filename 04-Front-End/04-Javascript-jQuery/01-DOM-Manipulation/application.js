759b7b0a998423daa70a3b2ca340dd19c34a9afe
Annabel <annabel.menard@gmail.com>
2014-11-03 11:42:19 +0100

    Exo1

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,30 +8,30 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
+  var email = $('#email').val();  // TODO: replace null, keep the email variable.
 
 
   // Ex 2. Fill the content of the email input with your email
-
+  var newEmail = $('#email').val('annabel.menard@gmail.com');
 
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+  var emailHint = $('#email-hint').html('<strong>This is my email now</strong>')
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+  var color = $('thead').addClass('blue');
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
+  var team_count = $('tbody > tr').length;  // TODO: replace 0, keep the team_count variable.
 
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
 
-
+  var add_row = $('tbody').append('<tr><td>15</td><td>New Team</td><td>0</td></tr>');
 
   // Ex 7. Write some code to sum all points given to all teams
   var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
