550eefa5e3cc707487a4c0ae015adb09b9d9ed2c
Annabel <annabel.menard@gmail.com>
2014-11-03 15:12:48 +0100

    Exo2

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,3 +1,21 @@
+
+$(function() {
+  $('#message').delay(500).slideDown(500, function(e) {
+    $(this).on('click', function() {
+      $(this).fadeOut(500);
+    });
+  });
+
+  var fadeInFunc = function() {
+      $('#message').fadeIn(500);
+  }
+
+  var fadeOutFunc = function() {
+      $('#message').fadeOut(500);
+  }
+
+  $('#message-wrapper').hover(fadeInFunc, fadeOutFunc);
+});
 /*
   *
   * TODO: add your code here!
